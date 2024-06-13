f262336c97d67a59033a8b7c3c6bd4d1c2ce61a3
soyguille1 <123403145+soyguille1@users.noreply.github.com>
2024-06-12 21:12:11 -0400

    Day 2
    Multiplicacion ingresado por los usuarios

---
+++ challenge2.py
@@ -0,0 +1,4 @@
+multiplicar = int(input("Cual es el numero que quiere multiplicar"))
+for multiplicacion in range(1,11):
+    resultado = multiplicar*multiplicacion
+    print(f"{multiplicar} * {multiplicacion} = {resultado} ")
