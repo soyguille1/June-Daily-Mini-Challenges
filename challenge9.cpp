dc42bb8336fea6d77b18783c958f6764af9a47bb
soyguille1 <123403145+soyguille1@users.noreply.github.com>
2024-06-21 23:12:08 -0400

    Day 9
    Suma de 2 numeros en c++

---
+++ challenge9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a;
+	int b;
+	int suma;
+	
+	cout << "Ingrese su primer numero" << endl;
+	cin >> a;
+	
+	cout << "Ingrese su segundo numero" << endl;
+	cin >> b;
+	
+	suma = a+b;
+	cout << "El resultado es: "<< suma <<endl;
+	return 0;
+}
